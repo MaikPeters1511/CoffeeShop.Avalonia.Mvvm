1d128c958d6c23abfbe3cfa49dcd2c4d1f7167d3
Maiki <bewerbung@maikpeters.org>
2024-10-04 19:24:10 +0200

    Add two-way binding for Customer properties
    Enhanced `CustomerItemViewModel` with setters and change notifications for properties. Updated `MainWindow.axaml` to configure two-way bindings, improve UI data flow, and display additional customer details.

--- ViewModels/CustomerItemViewModel.cs
+++ ViewModels/CustomerItemViewModel.cs
@@ -9,6 +9,57 @@
             Customer = customer ?? throw new ArgumentNullException(nameof(customer));
         }
 
-        public string? FirstName => Customer.FirstName;
+        public int Id
+        {
+            get => Customer.Id;
+            set
+            {
+                if (Customer.Id != value)
+                {
+                    Customer.Id = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string? FirstName
+        {
+            get => Customer.FirstName;
+            set
+            {
+                if (Customer.FirstName != value)
+                {
+                    Customer.FirstName = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string? LastName
+        {
+            get => Customer.LastName;
+            set
+            {
+                if (Customer.LastName != value)
+                {
+                    Customer.LastName = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public bool IsDeveloper
+        {
+            get => Customer.IsDeveloper;
+            set
+            {
+                if (Customer.IsDeveloper != value)
+                {
+                    Customer.IsDeveloper = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
     }
 }
